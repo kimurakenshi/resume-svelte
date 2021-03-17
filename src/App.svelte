e6a07c6302af4592fb7d8ba11c919f46d5e69e21
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-03-17 15:45:46 -0700

    vite with svelte and ts and scss

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,65 @@
 <script lang="ts">
-  import Header from "./Header.svelte";
-  import { count } from "./store";
-  function onHeaderClicked(e: CustomEvent) {
-    alert(e.detail.text);
-  }
-
-  function incrementCounter() {
-    count.update((n) => n + 1);
-  }
-
-  function resetCounter() {
-    count.set(0);
-  }
-
-  let countValue: number;
-
-  count.subscribe((counter) => (countValue = counter));
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<div>
-  <Header on:header-clicked={onHeaderClicked} />
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1>Hello Typescript!</h1>
 
-  <button on:click={incrementCounter}>Increment</button>
-  <button on:click={resetCounter}>Reset</button>
-  <span class="text-2xl font-bold">
-    {countValue}
-  </span>
-</div>
+  <Counter id="0" />
 
-<style global>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+<style lang="scss">
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
 </style>

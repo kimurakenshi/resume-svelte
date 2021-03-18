b1509be2cdd41709a6425859bfb6061ba05a06c3
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-03-18 09:56:45 -0700

    add windicss

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -12,18 +12,18 @@
   Clicks: {$count}
 </button>
 
-<style>
+<style lang="scss">
   button {
     font-family: inherit;
-    font-size: inherit;
     padding: 1em 2em;
     color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
     border-radius: 2em;
     border: 2px solid rgba(255, 62, 0, 0);
     outline: none;
     width: 200px;
     font-variant-numeric: tabular-nums;
+
+    @apply bg-red-100;
   }
 
   button:focus {
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+const colors = require('windicss/colors')
+const typography = require('windicss/plugin/typography')
+
+module.exports = {
+    darkMode: 'class',
+    plugins: [typography],
+    theme: {},
+}

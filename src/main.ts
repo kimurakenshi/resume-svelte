a210b501fae50249121d2a2a1aeec1e1660c1e23
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-03-18 15:22:01 -0700

    add routify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import App from './App.svelte';
 import 'virtual:windi.css';
+import HMR from '@roxi/routify/hmr';
+import App from './App.svelte';
 
-const app = new App({
-  target: document.body,
-});
+const app = HMR(App, { target: document.body });
 
 export default app;

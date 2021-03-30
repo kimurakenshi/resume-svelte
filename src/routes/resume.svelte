b6b17787443d87cff159ad573105f83156518852
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-03-30 15:18:32 -0700

    resume page

--- src/routes/resume.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Page from '$components/Page.svelte';
-</script>
-
-<Page description="Resume information" title="Resume">
-	<h1>Resume</h1>
-</Page>
--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,3 +1,5 @@
+const { mdsvex } = require('mdsvex');
+const mdsvexConfig = require('./mdsvex.config.cjs');
 const path = require('path');
 const sveltePreprocess = require('svelte-preprocess');
 const vercel = require('@sveltejs/adapter-vercel');
@@ -5,9 +7,11 @@ const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
+		mdsvex(mdsvexConfig),
 		sveltePreprocess({
 			defaults: {
 				style: 'postcss'

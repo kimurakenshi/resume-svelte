bc7d13d4d7d3af2447cc8862709c88b34c9cd7bc
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-03-30 12:53:26 -0700

    add page component and integrate to existing pages

---
+++ src/components/Page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let title = '';
+	export let description = '';
+	export let customClasses = '';
+	export let hasSpacing = true;
+	export let hasMarkdown = false;
+	export let imageIndex = 0;
+	export let items = [
+		{
+			id: 1,
+			classNames: 'background on-background'
+		}
+	];
+</script>
+
+<svelte:head>
+	<meta name="Description" content={description} />
+	<title>{title}</title>
+</svelte:head>
+<!--	@todo: remove background class from here once parallax is implemented-->
+<div class="background on-background font-default relative ">
+	<!--	@todo: it depends on Background component-->
+	<!--{items && items.length > 0 && (-->
+	<!--  <Background currentItemIndex={imageIndex} items={items} />-->
+	<!--)}-->
+
+	<main
+		class={hasSpacing
+			? `on-background page--min-height py-6 md:py-16 px-8 mx-auto my-0 max-w-full md:max-w-2xl xl:max-w-5xl 2xl:max-w-6xl ${customClasses}`
+			: `on-background page--min-height ${customClasses}`}
+	>
+		{#if hasMarkdown}
+			<article class="prose dark:prose--dark max-w-none">
+				<slot />
+			</article>
+		{:else}
+			<slot />
+		{/if}
+	</main>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,12 @@
-<h1>Home</h1>
+<script lang="ts">
+	import { theme, ThemeName } from '$core/theme';
+	import Page from '$components/Page.svelte';
+</script>
+
+<Page description="Home" title="Home" hasSpacing={false}>
+	{#if $theme === ThemeName.LIGHT}
+		<div>It is the light theme</div>
+	{:else}
+		<div>It is the dark theme</div>
+	{/if}
+</Page>

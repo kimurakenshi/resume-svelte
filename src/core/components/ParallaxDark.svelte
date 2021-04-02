fe376070d4ba726652fdca7fbd031c1da6c92763
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-04-02 13:11:53 -0700

    fix background color on body

--- src/core/components/ParallaxDark.svelte
+++ src/core/components/ParallaxDark.svelte
@@ -3,6 +3,8 @@
 	import Panel from '$components/Panel.svelte';
 	import Text from '$components/Text.svelte';
 	import { onMount } from 'svelte';
+	import Parallax from '../../components/Parallax/Parallax.svelte';
+	import ParallaxLayer from '../../components/Parallax/ParallaxLayer.svelte';
 
 	let showTypewriterAnimation = false;
 	const typeWriterClasses =
@@ -13,28 +15,38 @@
 	});
 </script>
 
-{#if showTypewriterAnimation}
-	<div class="page--min-height flex flex-col items-center justify-center">
-		<Typewriter classes={typeWriterClasses} delay={2}>Hello! , I'm Sebastián.</Typewriter>
+<Parallax>
+	<ParallaxLayer>
+		{#if showTypewriterAnimation}
+			<div class="page--min-height flex flex-col items-center justify-center">
+				<Typewriter classes={typeWriterClasses} delay={2}>Hello! , I'm Sebastián.</Typewriter>
 
-		<Typewriter classes={typeWriterClasses} delay={4}>This is my personal Website.</Typewriter>
-	</div>
-{/if}
+				<Typewriter classes={typeWriterClasses} delay={4}>This is my personal Website.</Typewriter>
+			</div>
+		{/if}
+	</ParallaxLayer>
 
-<Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
-	<Text>
-		I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to succeed.
-	</Text>
-</Panel><Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
-	<Text>
-		I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to succeed.
-	</Text>
-</Panel><Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
-	<Text>
-		I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to succeed.
-	</Text>
-</Panel><Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
-	<Text>
-		I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to succeed.
-	</Text>
-</Panel>
+	<ParallaxLayer>
+		<Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
+			<Text>
+				I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to
+				succeed.
+			</Text>
+		</Panel><Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
+			<Text>
+				I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to
+				succeed.
+			</Text>
+		</Panel><Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
+			<Text>
+				I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to
+				succeed.
+			</Text>
+		</Panel><Panel customClasses="mb-10 md:mb-20 bg-gray-800 bg-opacity-60">
+			<Text>
+				I enjoy building new things, I&apos;m passionated about UI/UX and helping my team to
+				succeed.
+			</Text>
+		</Panel>
+	</ParallaxLayer>
+</Parallax>

34c310a57feb54bb5825f9d962f61a1c897d4894
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-04-01 17:34:20 -0700

    initial version of homep age

---
+++ src/components/Typewriter.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let delay = 2;
+	export let classes = '';
+
+	function typewriter(node, { speed = 50 }) {
+		const text = node.textContent;
+		const duration = text.length * speed;
+
+		return {
+			delay: delay * 1000,
+			duration,
+			tick: (t) => {
+				const i = ~~(text.length * t);
+
+				node.textContent = text.slice(0, i);
+			}
+		};
+	}
+</script>
+
+<p class={classes} in:typewriter><slot /></p>
